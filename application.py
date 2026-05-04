0b7fcdac8f7e0b81796c6aad4f99dc5f9fbea977
Payal-Agarwal <46149523+Payal-Agarwal@users.noreply.github.com>
2026-05-04 21:32:53 +0530

    Refactor home route response in application.py
    Removed unnecessary newlines and fixed return message.

--- application.py
+++ application.py
@@ -4,8 +4,4 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "CI/CD working successfully on AWS Elastic Beanstalk!"
-
-## IMPORTANT for AWS
-if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=8000)
+    return "CI/CD working successfully on AWS Elastic Beanstalk"
